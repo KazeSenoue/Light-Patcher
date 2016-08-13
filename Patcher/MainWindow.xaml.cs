629a72f0573f05da2b18379b30baf67df42a4ba7
Rodrigo Leite <kazesenoue@gmail.com>
2016-08-13 00:46:41 -0300

    First implementation of the user interface
    Obviously not the final design, and only functionality is select
    pso2_bin folder and save it to settings.json. So far :eyes:

--- Patcher/MainWindow.xaml.cs
+++ Patcher/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -23,6 +26,33 @@ namespace Patcher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //Loads settings
+            Settings set = new Settings();
+            Settings settings = set.ReturnSettings();
+
+            //Prompts user to select pso2_dir
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+
+            //If the folder is "pso2_bin", saves it to settings.json. If not, tells the user they fucked up.
+            var pso2_dir = dialog.FileName;
+            Console.WriteLine(pso2_dir);
+            if (pso2_dir.EndsWith("pso2_bin"))
+            {
+                settings.PSO2 = pso2_dir;
+                string output = JsonConvert.SerializeObject(settings, Formatting.Indented);
+                File.WriteAllText("settings.json", output);
+                MessageBox.Show(String.Format("Saved!\nSelected folder: {0}", settings.PSO2));
+            }
+            else
+            {
+                MessageBox.Show(String.Format("{0} is not a valid PSO2 folder. Please try again.", pso2_dir));
+            }
         }
     }
 }
---
+++ Patcher/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patcher
+{
+    class Settings
+    {
+        [JsonRequired]
+        [JsonProperty("PSO2")]
+        public string PSO2 { get; set; }
+
+        //Loads settings
+        //Settings settings = new Settings();
+        //Settings obj = settings.ReturnSettings();
+
+        public Settings ReturnSettings()
+        {
+            string settingsFilePath = "settings.json";
+
+            if (!File.Exists(settingsFilePath))
+            {
+                Console.WriteLine("Settings file doesn't exist.");
+                throw new Settings.MissingFileException();
+            }
+
+            Settings obj = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(settingsFilePath));
+            return obj;
+        }
+
+        public class MissingFileException: Exception
+        {
+            public MissingFileException()
+            {
+            }
+
+        }
+    }
+}
