7bc19988d9b5abe3202550b5e02c3e56ca2c11e3
KazeSenoue <kazesenoue@gmail.com>
2017-03-30 03:42:36 -0300

    Here we go

--- Main/MainWindow.xaml.cs
+++ Main/MainWindow.xaml.cs
@@ -2,22 +2,13 @@
 using System.IO;
 using System.Diagnostics;
 using System.Net;
-using System.IO.Compression;
 using SharpCompress.Archives;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using SharpCompress.Readers;
 using System.ComponentModel;
 
