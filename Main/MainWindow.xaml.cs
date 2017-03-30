84d6e5a1881c027cd54aff85dd9ebe382b92c369
KazeSenoue <kazesenoue@gmail.com>
2017-03-29 22:42:25 -0300

    Added english patch downloading functionality

--- Main/MainWindow.xaml.cs
+++ Main/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+using System.IO.Compression;
+using SharpCompress.Archives;
+using Newtonsoft.Json;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +18,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SharpCompress.Readers;
 
 namespace Main
 {
@@ -20,9 +27,79 @@ namespace Main
     /// </summary>
     public partial class MainWindow : Window
     {
+        Settings settings = new Settings().ReturnSettings();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void launch_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(settings.pso2_path + "/pso2.exe"))
+            {
+                var info = new ProcessStartInfo(settings.pso2_path + "/pso2.exe");
+                info.EnvironmentVariables.Add("-pso2", "+0x01e3f1e9");
+                info.Arguments = "+0x33aca2b9";
+                info.UseShellExecute = false;
+
+                var process = new Process();
+                process.StartInfo = info;
+                process.Start();
+            }
+        }
+
+        public async Task<Dictionary<string, Dictionary<string, string>[]>> GetDictAsync(string uri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var content = await client.GetStringAsync(uri);
+                return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>[]>>(content);
+            }
+        }
+
+        public async Task GetFileAsync(string uri, string location)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+
+                Directory.CreateDirectory(location);
+                using (FileStream fileStream = new FileStream(Path.Combine(location, Path.GetFileName(uri)), FileMode.CreateNew, FileAccess.Write, FileShare.None))
+                {
+                    await response.Content.CopyToAsync(fileStream);
+                }
+            }
+        }
+
+        private async void enpatch_button_Click(object sender, RoutedEventArgs e)
+        {
+            pb_label.Content = "Installing english patch...";
+
+            string url = "https://pitchblack.arghlex.net/pso2/?sort=modtime&order=desc&json";
+            string base_url = "https://pso2.arghlex.net/pso2/";
+
+            var json = await GetDictAsync(url);
+            var file = json["files"][0]["name"];
+
+            string file_url = base_url + file;
+            await GetFileAsync(file_url, "Temp");
+
+            using (var archive = SharpCompress.Archives.Zip.ZipArchive.Open(Path.Combine("Temp/", file)))
+            {
+                foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
+                {
+                    entry.WriteToDirectory(Path.Combine(settings.pso2_path, @"data/win32"), new ExtractionOptions()
+                    {
+                        ExtractFullPath = true,
+                        Overwrite = true
+                    });
+                }
+            }
+
+            Directory.Delete("Temp", true);
+            pb_label.Content = "Success!";
         }
     }
 }
---
+++ Main/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Main
+{
+    class Settings
+    {
+        [JsonRequired]
+        [JsonProperty("pso2_path")]
+        public string pso2_path { get; set; }
+
+        public Settings ReturnSettings()
+        {
+            string settings_file_path = "settings.json";
+
+            if (!File.Exists(settings_file_path))
+            {
+                Settings _data = new Settings();
+                _data.pso2_path = "";
+
+                string json = JsonConvert.SerializeObject(_data);
+                File.WriteAllText(settings_file_path, json);
+
+                return _data;
+            }
+
+            Settings obj = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(settings_file_path));
+            return obj;
+        }
+
+    }
+}
